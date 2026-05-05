2c9005739e43d34215cd76f421d304093256e5a4
reeyooh <riyotom9@gmail.com>
2026-05-05 16:20:57 +0530

    Update dboperation.php

--- dboperation.php
+++ dboperation.php
@@ -6,22 +6,21 @@ class dboperation
     function __construct()
     {
         $this->con = mysqli_connect(
-            "localhost",              // IMPORTANT (works only inside InfinityFree)
-            "if0_41835845",           // your username
-            "infinitycarhub",          // your DB password
+            "localhost",              // IMPORTANT (InfinityFree uses localhost)
+            "if0_41835845",           // your DB username
+            "YOUR_PASSWORD",          // your DB password
             "if0_41835845_carhub"     // your DB name
         );
 
         if (!$this->con)
         {
-            die("Connection failed:" . mysqli_connect_error());
+            die("Connection failed: " . mysqli_connect_error());
         }
     }
 
     public function executequery($sqlquery)
     {
-        $this->result = mysqli_query($this->con, $sqlquery);
-        return $this->result;
+        return mysqli_query($this->con, $sqlquery);
     }
 }
 ?>
